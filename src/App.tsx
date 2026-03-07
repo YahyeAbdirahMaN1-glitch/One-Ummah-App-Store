3746e90ff616024d1fcc0ceebfdf730d08ad4eb0
YahyeAbdirahMaN1-glitch <YahyeAbdirahMaN1@hotmail.com>
2026-03-07 15:37:14 -0500

    fix(navigation): rebuild layout to restore back button and bottom tabs  - Replaced Layout.tsx with fixed bottom navigation - Ensured all protected pages render inside Layout - Added padding to prevent content from covering bottom tabs - Fixed navigation so users can leave Prayer Times page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from './components/ui/sonner';
+
 import Layout from './components/Layout';
+
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
@@ -30,33 +32,50 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+
         <Routes>
-          {/* Public routes */}
+
+          {/* Public Routes */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/privacy" element={<PrivacyPolicyPage />} />
           <Route path="/terms" element={<TermsOfServicePage />} />
 
-          {/* Protected routes inside Layout */}
+          {/* Protected Routes with Layout */}
           <Route element={<Layout />}>
+
             <Route path="/" element={<HomePage />} />
+
             <Route path="/prayer-times" element={<PrayerTimesPage />} />
-            <Route path="/profile-setup" element={<ProfileSetupPage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
-            <Route path="/profile-settings" element={<ProfileSettingsPage />} />
-            <Route path="/privacy-settings" element={<PrivacySettingsPage />} />
+
+            <Route path="/friends" element={<FriendsPage />} />
+
             <Route path="/messages" element={<MessagesPage />} />
             <Route path="/messages/:userId" element={<MessagesPage />} />
-            <Route path="/friends" element={<FriendsPage />} />
+
+            <Route path="/profile-setup" element={<ProfileSetupPage />} />
+
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+
+            <Route path="/profile-settings" element={<ProfileSettingsPage />} />
+
+            <Route path="/privacy-settings" element={<PrivacySettingsPage />} />
+
             <Route path="/settings" element={<SettingsPage />} />
+
             <Route path="/report-problem" element={<ReportProblemPage />} />
+
           </Route>
 
           {/* Catch all */}
           <Route path="*" element={<Navigate to="/" replace />} />
+
         </Routes>
+
       </BrowserRouter>
+
       <Toaster />
+
     </QueryClientProvider>
   );
 }
